2570adc56b616184bca169c3fc9fc03815a1e44b
ewudata <mega.photo@gmail.com>
2016-10-09 19:46:23 -0700

    ZZ

--- lab0/bootstrap.sh
+++ lab0/bootstrap.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-yum -y -q update
-yum -y install epel-release
+yum -y -q install epel-release
+# yum -y -q update
+
+echo "
+## Colorize the ls output ##
+alias ls='ls --color=auto'
+
+## get rid of command not found ##
+alias cd..='cd ..'
+ 
+## a quick way to get out of current directory ##
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+alias mkdir='mkdir -pv'
+" >> /home/vagrant/.bash_profile
